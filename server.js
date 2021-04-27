4fcb85556104a5392a4134f5d18142b203547e12
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-04-27 20:03:21 +0600

    added ejs module and index.ejs

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log('...server.js start here:');
+console.log('...server.js started here:');
 
 require('dotenv').config()
 
@@ -6,5 +6,20 @@ const hostname =  process.env.HOST;
 const database =  process.env.DATABASE;
 const port =  process.env.PORT;
 
+///Start with express
+const express = require('express')
 
-console.log(hostname);
+const app = express();
+
+app.set('view-engine', 'ejs');
+
+
+//Sets up render response for local host
+app.get('/', (req,res)=>{
+    // console.log('res', res)
+    res.render('index.ejs', { name:'Awshaf'}); 
+})
+
+app.listen(3000);
+
+console.log('...end line');
